2a786bf11b2c2dc02c9ebbaf6a51c9c8634d8eb0
Paul Schuetz <paul.schuetz@inovex.de>
2023-08-17 13:57:19 +0200

    switch to ktor client instead of skrapeit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val ktor_version: String by project
+
 plugins {
-    kotlin("jvm") version "1.7.21"
-    kotlin("plugin.serialization") version "1.7.20"
+    id("org.graalvm.buildtools.native") version "0.9.8"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.serialization") version "1.8.20"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     application
 }
@@ -12,21 +15,52 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 
 dependencies {
-    implementation("it.skrape:skrapeit:1.1.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:1.4.6")
     testImplementation(kotlin("test"))
+}
+
+graalvmNative {
+    binaries {
+        named("main"){
+            javaLauncher.set(javaToolchains.launcherFor {
+                languageVersion.set(JavaLanguageVersion.of(17))
+            })
+
+            fallback.set(false)
+            verbose.set(true)
+
+            buildArgs.add("--initialize-at-build-time=io.ktor,kotlin,kotlinx.serialization,ch.qos.logback,kotlinx.coroutines,org.slf4j.LoggerFactory")
+
+            buildArgs.add("-H:+InstallExitHandlers")
+            buildArgs.add("-H:+ReportUnsupportedElementsAtRuntime")
+            buildArgs.add("-H:+ReportExceptionStackTraces")
+            buildArgs.add("--trace-class-initialization=ch.qos.logback.core.status.InfoStatus")
+        }
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
 }
 
 application {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,16 @@
-import it.skrape.fetcher.HttpFetcher
-import it.skrape.fetcher.Method
-import it.skrape.fetcher.response
-import it.skrape.fetcher.skrape
-import kotlinx.serialization.*
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
@@ -16,84 +24,133 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 
 class Function {
+
+    private val client: HttpClient
+
+    @OptIn(ExperimentalSerializationApi::class)
+    private val json: Json = Json {
+        explicitNulls = false
+        encodeDefaults = false
+        isLenient = true
+        ignoreUnknownKeys = true
+    }
+
+    init {
+        client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(json)
+            }
+        }
+    }
+
+    @Serializable
+    data class GatewayResponse(
+        val statusCode: Int, val body: String
+    )
+
     @OptIn(ExperimentalSerializationApi::class)
     fun handler(input: InputStream, output: OutputStream) {
         val inputRaw = input.bufferedReader().use { it.readText() }
         println("Received $inputRaw")
-        val inputJson = Json.parseToJsonElement(inputRaw)
-        val from = inputJson.jsonObject["queryStringParameters"]?.jsonObject?.get("from")?.jsonPrimitive?.contentOrNull
-            ?: "Severinskirche"
-        val to = inputJson.jsonObject["queryStringParameters"]?.jsonObject?.get("to")?.jsonPrimitive?.contentOrNull
-            ?: "Severinstr."
-        Json.encodeToStream(ConnectionResponse(connections = fetchConnections(from, to)), output)
+
+        val inputJson = json.parseToJsonElement(inputRaw)
+        val from =
+            inputJson.jsonObject["queryStringParameters"]?.handleJsonNull()?.jsonObject?.get("from")?.jsonPrimitive?.contentOrNull
+                ?: "Severinskirche"
+        val to =
+            inputJson.jsonObject["queryStringParameters"]?.handleJsonNull()?.jsonObject?.get("to")?.jsonPrimitive?.contentOrNull
+                ?: "Severinstr."
+
+        Json.encodeToStream(
+            serializer = GatewayResponse.serializer(),
+            value = GatewayResponse(
+                statusCode = 200, body = Json.encodeToString(
+                    value = ConnectionResponse(connections = runBlocking { fetchConnections(from, to) }),
+                    serializer = ConnectionResponse.serializer()
+                )
+            ),
+            stream = output
+        )
     }
 
-    fun fetchConnections(from: String, to: String): List<Connection> {
+    private fun JsonElement.handleJsonNull(): JsonElement? = when (this) {
+        is JsonNull -> null
+        else -> this
+    }
+
+    @OptIn(ExperimentalTime::class)
+    suspend fun fetchConnections(from: String, to: String): List<Connection> {
         val berlin = ZoneId.of("Europe/Berlin")
+        val now = { LocalDateTime.now(berlin) }
 
-        val date = LocalDate.now(berlin).format(DateTimeFormatter.ofPattern("YYYYMMdd"))
-        val time = LocalDateTime.now(berlin).format(DateTimeFormatter.ofPattern("HHmmss"))
+        val date = now().format(DateTimeFormatter.ofPattern("YYYYMMdd"))
+        val time = now().format(DateTimeFormatter.ofPattern("HHmmss"))
 
-        return skrape(HttpFetcher) {
-            request {
-                method = Method.POST
-                url = "https://auskunft.kvb.koeln/gate"
-                headers = mapOf("accept" to "application/json", "content-type" to "application/json")
-                sslRelaxed = true
-                body = """
-                { "ver": "1.58", "lang": "deu", "auth": { "type": "AID", "aid": "Rt6foY5zcTTRXMQs" }, "client": { "id": "HAFAS", "type": "WEB", "name": "webapp", "l": "vs_webapp" }, "formatted": false, "svcReqL": [ { "meth": "TripSearch", "req": { "jnyFltrL": [ { "type": "GROUP", "mode": "INC", "value": "RQ_CLIENT" }, { "type": "META", "mode": "INC", "meta": "TRIP_SEARCH_CHG_PRF_STD" }, { "type": "PROD", "mode": "INC", "value": 155 } ], "getPolyline": false, "getPasslist": false, "depLocL":[{"name":"${from}"}],"arrLocL":[{"name":"${to}"}], "outFrwd": true, "outTime": "$time", "outDate": "$date", "ushrp": false, "liveSearch": true, "maxChg": "1000", "minChgTime": "-1" }} ] }
-            """.trimIndent()
-            }
-            response {
-                val jsonTree = Json.decodeFromString<JsonObject>(this.responseBody)
-
-                return@response jsonTree["svcResL"]!!.jsonArray[0].jsonObject["res"]!!.jsonObject["outConL"]!!.jsonArray.map { arrElement ->
-                    val duration = arrElement.jsonObject["dur"]!!.jsonPrimitive.content
-                    val arrival = arrElement.jsonObject["arr"]!!.jsonObject["aTimeR"]?.jsonPrimitive?.contentOrNull
-                        ?: arrElement.jsonObject["arr"]!!.jsonObject["aTimeS"]?.jsonPrimitive?.content!!
-                    val departure = arrElement.jsonObject["dep"]!!.jsonObject["dTimeR"]?.jsonPrimitive?.contentOrNull
-                        ?: arrElement.jsonObject["dep"]!!.jsonObject["dTimeS"]?.jsonPrimitive?.content!!
-                    val changes = arrElement.jsonObject["chg"]!!.jsonPrimitive.int
-                    val steps = arrElement.jsonObject["secL"]!!.jsonArray.map { step ->
-                        val departureTime = step.jsonObject["dep"]!!.jsonObject["dTimeR"]?.jsonPrimitive?.contentOrNull
-                            ?: step.jsonObject["dep"]!!.jsonObject["dTimeS"]!!.jsonPrimitive.content
-                        val arrivalTime = step.jsonObject["arr"]!!.jsonObject["aTimeR"]?.jsonPrimitive?.contentOrNull
-                            ?: step.jsonObject["arr"]!!.jsonObject["aTimeS"]!!.jsonPrimitive.content
-                        if (step.jsonObject["type"]!!.jsonPrimitive.contentOrNull == "JNY") {
-                            val regex = Regex(""".+@O=(.+?)@.+@O=(.+?)@.+\s{4,7}(.+?)\$.+""")
-                            val (_, start, dest, mobility) = regex.matchEntire(step.jsonObject["jny"]!!.jsonObject["ctxRecon"]!!.jsonPrimitive.contentOrNull!!)!!.groupValues
-                            ConnectionStep(
-                                mobilityName = mobility,
-                                start = start,
-                                destination = dest,
-                                departureTime = parseTime(departureTime).atZone(berlin).toEpochSecond(),
-                                arrivalTime = parseTime(arrivalTime).atZone(berlin).toEpochSecond()
-                            )
-                        } else {
-                            ConnectionStep(
-                                mobilityName = "Walking",
-                                departureTime = parseTime(departureTime).atZone(berlin).toEpochSecond(),
-                                arrivalTime = parseTime(arrivalTime).atZone(berlin).toEpochSecond()
-                            )
-                        }
+        val response = measureTimedValue {
+            withContext(Dispatchers.IO) {
+                client.post {
+                    url("https://auskunft.kvb.koeln/gate")
+                    headers {
+                        this.append("accept", "application/json")
+                        this.append("content-type", "application/json")
                     }
+                    setBody("""{ "ver": "1.58", "lang": "deu", "auth": { "type": "AID", "aid": "Rt6foY5zcTTRXMQs" }, "client": { "id": "HAFAS", "type": "WEB", "name": "webapp", "l": "vs_webapp" }, "formatted": false, "svcReqL": [ { "meth": "TripSearch", "req": { "jnyFltrL": [ { "type": "GROUP", "mode": "INC", "value": "RQ_CLIENT" }, { "type": "META", "mode": "INC", "meta": "TRIP_SEARCH_CHG_PRF_STD" }, { "type": "PROD", "mode": "INC", "value": 155 } ], "getPolyline": false, "getPasslist": false, "depLocL":[{"name":"${from}"}],"arrLocL":[{"name":"${to}"}], "outFrwd": true, "outTime": "$time", "outDate": "$date", "ushrp": false, "liveSearch": true, "maxChg": "1000", "minChgTime": "-1" }} ] }""".trimIndent())
+                }
+            }
+        }.also {
+            println("http request took ${it.duration.inWholeMilliseconds} milliseconds")
+        }.value
 
-                    Connection(
-                        departure = parseTime(departure),
-                        arrival = parseTime(arrival, departure = parseTime(departure)),
-                        durationInMin = inMinutes(duration),
-                        timeUntilDepartureInSec = LocalDateTime.now(berlin)
-                            .until(parseTime(departure), ChronoUnit.SECONDS).toInt().let {
-                                if (it < 0) 0 else it
-                            },
-                        departureEpoch = parseTime(departure).atZone(berlin).toEpochSecond(),
-                        changes = changes,
-                        steps = steps
+        val responseBody = response.bodyAsText()
+        val jsonTree = json.parseToJsonElement(responseBody).jsonObject
+
+        return jsonTree["svcResL"]!!.jsonArray[0].jsonObject["res"]!!.jsonObject["outConL"]!!.jsonArray.map { arrElement ->
+            val duration = arrElement.jsonObject["dur"]!!.jsonPrimitive.content
+            val arrival = arrElement.jsonObject["arr"]!!.jsonObject["aTimeR"]?.jsonPrimitive?.contentOrNull
+                ?: arrElement.jsonObject["arr"]!!.jsonObject["aTimeS"]?.jsonPrimitive?.content!!
+            val departure = arrElement.jsonObject["dep"]!!.jsonObject["dTimeR"]?.jsonPrimitive?.contentOrNull
+                ?: arrElement.jsonObject["dep"]!!.jsonObject["dTimeS"]?.jsonPrimitive?.content!!
+            val changes = arrElement.jsonObject["chg"]!!.jsonPrimitive.int
+            val steps = arrElement.jsonObject["secL"]!!.jsonArray.map { step ->
+                val departureTime = step.jsonObject["dep"]!!.jsonObject["dTimeR"]?.jsonPrimitive?.contentOrNull
+                    ?: step.jsonObject["dep"]!!.jsonObject["dTimeS"]!!.jsonPrimitive.content
+                val arrivalTime = step.jsonObject["arr"]!!.jsonObject["aTimeR"]?.jsonPrimitive?.contentOrNull
+                    ?: step.jsonObject["arr"]!!.jsonObject["aTimeS"]!!.jsonPrimitive.content
+                if (step.jsonObject["type"]!!.jsonPrimitive.contentOrNull == "JNY") {
+                    val regex = Regex(""".+@O=(.+?)@.+@O=(.+?)@.+\s{4,7}(.+?)\$.+""")
+                    val (_, start, dest, mobility) = regex.matchEntire(step.jsonObject["jny"]!!.jsonObject["ctxRecon"]!!.jsonPrimitive.contentOrNull!!)!!.groupValues
+                    ConnectionStep(
+                        mobilityName = mobility,
+                        start = start,
+                        destination = dest,
+                        departureTime = parseTime(departureTime).atZone(berlin).toEpochSecond(),
+                        arrivalTime = parseTime(arrivalTime).atZone(berlin).toEpochSecond()
+                    )
+                } else {
+                    ConnectionStep(
+                        mobilityName = "Walking",
+                        departureTime = parseTime(departureTime).atZone(berlin).toEpochSecond(),
+                        arrivalTime = parseTime(arrivalTime).atZone(berlin).toEpochSecond()
                     )
                 }
             }
+
+            Connection(
+                departure = parseTime(departure),
+                arrival = parseTime(arrival, departure = parseTime(departure)),
+                durationInMin = inMinutes(duration),
+                timeUntilDepartureInSec = LocalDateTime.now(berlin).until(parseTime(departure), ChronoUnit.SECONDS)
+                    .toInt().let {
+                        if (it < 0) 0 else it
+                    },
+                departureEpoch = parseTime(departure).atZone(berlin).toEpochSecond(),
+                changes = changes,
+                steps = steps
+            )
         }
     }
 }
@@ -101,13 +158,12 @@ class Function {
 private fun inMinutes(duration: String) =
     duration.subSequence(0 until 2).toString().toInt() * 60 + duration.subSequence(2 until 4).toString().toInt()
 
-private fun parseTime(time: String, departure: LocalDateTime? = null): LocalDateTime = LocalDateTime.now()
-    .withNano(0)
-    .withHour(time.subSequence(0 until 2).toString().toInt())
-    .withMinute(time.subSequence(2 until 4).toString().toInt())
-    .withSecond(time.subSequence(4 until 6).toString().toInt()).let {
-        if (departure != null && departure > it) it.plusDays(1) else it
-    }
+private fun parseTime(time: String, departure: LocalDateTime? = null): LocalDateTime =
+    LocalDateTime.now().withNano(0).withHour(time.subSequence(0 until 2).toString().toInt())
+        .withMinute(time.subSequence(2 until 4).toString().toInt())
+        .withSecond(time.subSequence(4 until 6).toString().toInt()).let {
+            if (departure != null && departure > it) it.plusDays(1) else it
+        }
 
 
 @Serializable
@@ -119,21 +175,15 @@ data class ConnectionResponse(
 data class ConnectionStep(
     // Name of bus or train or "walking"
     val mobilityName: String,
-
-    val start: String? = null,
-    val departureTime: Long,
-
-    val destination: String? = null,
-    val arrivalTime: Long
+    val start: String? = null, val departureTime: Long,
+    val destination: String? = null, val arrivalTime: Long
 )
 
 @Serializable
 data class Connection(
-    @Serializable(with = LocalDateTimeSerializer::class)
-    val departure: LocalDateTime,
+    @Serializable(with = LocalDateTimeSerializer::class) val departure: LocalDateTime,
     val departureEpoch: Long,
-    @Serializable(with = LocalDateTimeSerializer::class)
-    val arrival: LocalDateTime,
+    @Serializable(with = LocalDateTimeSerializer::class) val arrival: LocalDateTime,
     val steps: List<ConnectionStep>,
     val timeUntilDepartureInSec: Int,
     val durationInMin: Int,
@@ -153,12 +203,30 @@ object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
 }
 
 fun main() {
-    println(
-        Json.encodeToString(
-            Function().fetchConnections(
-                from = "Severinskirche",
-                to = "Neumarkt"
-            )
+    println(Json.encodeToString(serializer = ListSerializer(Connection.serializer()), value = runBlocking {
+        Function().fetchConnections(
+            from = "Severinstr.", to = "Mülheim Wiener Platz"
         )
-    )
+    }))
+
+//    val json = Json {
+//        explicitNulls = false
+//        encodeDefaults = false
+//        isLenient = true
+//        ignoreUnknownKeys = true
+//    }
+//
+//    val client = HttpClient(CIO) {
+//        install(ContentNegotiation) {
+//            json(json)
+//        }
+//    }
+//
+//    val connection = Connection(departure = LocalDateTime.now(), departureEpoch = 1L, arrival = LocalDateTime.now(), steps = emptyList(), timeUntilDepartureInSec = 1, durationInMin = 2, changes = 3)
+//    val connectionJson = json.encodeToString(serializer = Connection.serializer(), connection)
+//    val back = json.parseToJsonElement(connectionJson)
+//
+//    println(back.jsonObject["changes"]!!.jsonPrimitive.int)
+//    runBlocking { test() }
+//    println("Hello world: $back")
 }
